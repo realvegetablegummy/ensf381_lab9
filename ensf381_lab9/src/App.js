05e8057dc8d560c7237c4800f12bb223d84ee80b
realvegetablegummy <144979434+realvegetablegummy@users.noreply.github.com>
2025-03-28 22:40:58 -0600

    added price route

--- ensf381_lab9/src/App.js
+++ ensf381_lab9/src/App.js
@@ -3,12 +3,14 @@ import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './Login';
+import HousePricePredictor from './HousePricePredictor';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login />} />
+        <Route path="/predict" element={<HousePricePredictor />} />
       </Routes>
     </BrowserRouter>
   );
